f98bb64165912d9a72eb34e11cfa3173efdd8245
Erik Fredner <fredner@stanford.edu>
2020-01-14 13:59:25 -0500

    remove make_cv.sh

--- make_cv.sh
+++
@@ -1,7 +0,0 @@
-# this script generates and opens pdf output of current version of cv
-# it also generates a current version of the html of the cv
-# should be executed from the root directory containing cv.md
-# requires pandoc
-pandoc -o cv.html cv.md
-pandoc cv.md -s -o cv.pdf
-open cv.pdf
